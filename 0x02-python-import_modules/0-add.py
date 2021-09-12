f0f34bcb284b1a824439848a3d49823833a0bb24
Barnabas27 <bnyambura032@gmail.com>
2021-09-12 09:40:03 +0300

    add modifications

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-import add_0 as ad
-
-a = 1
-b = 2
-res = ad.add(a, b)
-print("{} + {} = {}".format(a, b, res))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
