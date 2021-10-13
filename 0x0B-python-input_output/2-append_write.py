4aff4f1c6df33dc357da0587249495488501416e
Barnabas27 <bnyambura032@gmail.com>
2021-10-14 01:27:26 +0300

    add modifications

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Module 3-write_file
-Contains function that writes to text file and returns num chars written
+Module 4-append_write
+Contains function that appends to text file and returns num chars added
 """
 
 
-def write_file(filename="", text=""):
-    """writes to text file and returns num chars written"""
-    with open(filename, mode="w", encoding="utf-8") as f:
+def append_write(filename="", text=""):
+    """appends to text file and returns num chars added"""
+    with open(filename, mode="a", encoding="utf-8") as f:
         return(f.write(text))
