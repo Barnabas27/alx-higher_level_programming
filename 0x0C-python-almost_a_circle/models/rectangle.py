443cb874b34f025629cece72334e20e19c5e394d
Barnabas27 <bnyambura032@gmail.com>
2021-10-19 02:03:46 +0300

    add modifications

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,10 @@ from models.base import Base
 class Rectangle(Base):
     """ Rectangle inherits from Base"""
 
+    KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+               'height': '_Rectangle__height',
+               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Instantiation of instance attributes"""
 
