e2dac3dd71791985ec851697fdaa6b1e9c2f5e37
Barnabas27 <bnyambura032@gmail.com>
2021-12-14 20:25:06 +0300

    add a script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = process.argv.slice(2);
+if (parseInt(x[0])) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
