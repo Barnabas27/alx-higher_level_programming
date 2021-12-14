8313417aab04275b1feb6ea4c66f89eca86e2ae9
Barnabas27 <bnyambura032@gmail.com>
2021-12-14 20:02:13 +0300

    add a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+let count = 0;
+while (count < myLangs.length) {
+    console.log(myLangs[count]);
+    count++;
+}
