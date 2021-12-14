70327d84120229a1f07a154c7e91461eee11a19b
Barnabas27 <bnyambura032@gmail.com>
2021-12-14 20:19:22 +0300

    add modifications

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-let count = 0;
-while (count < myLangs.length) {
-  console.log(myLangs[count]);
-  count++;
+let i = 0;
+while (i < myLangs.length) {
+  console.log(myLangs[i]);
+  i++;
 }
