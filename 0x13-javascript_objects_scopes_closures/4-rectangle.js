acff146ba1d56f8cbeb8258511a8fe631418631e
Barnabas27 <bnyambura032@gmail.com>
2021-12-21 12:22:31 +0300

    add class with instance methods print(), rotate() and double()

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let sides = '';
+    for (let j = 0; j < this.width; j++) {
+      sides += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(sides);
+    }
+  }
+
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
